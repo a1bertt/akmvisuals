3a4818aff1b17bb4233297be2725cbc5c020c712
a1bertt <a1bert@hotmail.co.uk>
2025-08-10 15:35:13 +0100

    new images added to carousel

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const totalSlides = slides.length;
 let isTransitioning = false;
 let slideInterval = setInterval(nextSlide, 4000);
 
-// Clone the first and last slides for seamless transition
 const gallery = document.querySelector(".gallery");
-const firstSlideClone = slides[0].cloneNode(true);
-const lastSlideClone = slides[totalSlides - 1].cloneNode(true);
-gallery.appendChild(firstSlideClone); // Add first clone at the end
-gallery.insertBefore(lastSlideClone, slides[0]); // Add last clone at the start
+
+// Only add clones if they aren't already there
+if (!document.querySelector(".first-clone")) {
+  const firstSlideClone = slides[0].cloneNode(true);
+  const lastSlideClone = slides[totalSlides - 1].cloneNode(true);
+  firstSlideClone.classList.add("first-clone");
+  lastSlideClone.classList.add("last-clone");
+
+  gallery.appendChild(firstSlideClone); // Add first clone at the end
+  gallery.insertBefore(lastSlideClone, slides[0]); // Add last clone at the start
+}
 
 // Slide position update
 function updateSlidePosition(instant = false) {
